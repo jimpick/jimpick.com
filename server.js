274a3c4ae7d7a5f45b850e662d4ac1c676c4146c
Jim Pick <jim@joyent.com>
2011-03-26 20:07:57 -0700

    Add ejs templates, port home page.
    Still need to do contact page, manifest, static files.

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ express = require('express');
 
 var app = express.createServer();
 
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res){
-    res.send('Hello World');
+    res.render('index');
 });
 
 app.listen(process.env.PORT || 3000);
