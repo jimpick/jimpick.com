636e58c69a959d3134fdd32ec20b674bc2554063
Jim Pick <jim@jimpick.com>
2012-10-08 11:22:18 -0700

    Upgrade to Express 3.0
    Layouts disappeared!

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
-mime = require('mime');
-express = require('express');
+var mime = require('mime')
+  , express = require('express')
+  , app = express()
 
-var app = express.createServer();
+app.set('view engine', 'ejs')
 
-app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'))
 
-app.use(express.static(__dirname + '/public'));
+mime.define({'text/cache-manifest': ['manifest']})
 
-mime.define({'text/cache-manifest': ['manifest']});
+app.get('/', function(req, res) {
+  res.render('index')
+})
 
-app.get('/', function(req, res){
-  res.render('index');
-});
+app.get('/contact/?', function(req, res) {
+  res.render('contact')
+})
 
-app.get('/contact/?', function(req, res){
-  res.render('contact');
-});
-
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000)
 
